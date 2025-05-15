b8afb4af45a8dfe90053888d9577f2f774d2a3ab
Raman Sandhu <raman64854@gmail.com>
2025-05-15 00:24:22 -0700

    testing

--- server.js
+++ server.js
@@ -1,21 +1,12 @@
-var http = require('http');
-var url = require('url');
-var fs = require('fs');
- 
-http.createServer(function (req, res) {
-  var q = url.parse(req.url, true);
-  var filename = q.pathname == '/'? `./index.html` : `.${q.pathname}.html`;
-  fs.readFile(filename, function(err, data) {
-    if (err) { 
-        fs.readFile('./404.html', function(err, data) { 
-        res.writeHead(404, {'Content-Type': 'text/html'});
-        res.write(data)
-        return res.end();
-      }); 
-    return;
-    } 
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    return res.end();
-  });
-}).listen(8080);
+const express = require("express");
+const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
+
+app.get("/", (req, res) => res.send("Hello, world!"));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`My first Express app - listening on port ${PORT}!`);
+});
