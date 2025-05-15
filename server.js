981029ced1c0b3abfb682a9a967c9388b57e344c
Raman Sandhu <raman64854@gmail.com>
2025-05-15 00:39:48 -0700

    Refactor server routes and error handling

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 const express = require("express");
 const app = express();
-const dotenv = require('dotenv');
-dotenv.config();
 
-app.get("/", (req, res) => res.send("Hello, world!"));
+function serveIndeXFile(req, res){
+  res.sendFile(__dirname + "/index.html");
+}
+app.get("/", serveIndeXFile);
+app.get("/index", serveIndeXFile);
+app.get("/contact-me", (req, res) => {
+  res.sendFile(__dirname + "/contact-me.html");
+});
 
-const PORT = process.env.PORT || 5000;
+app.get("/about", (req, res) => {
+  res.sendFile(__dirname + "/about.html");
+});
 
-app.listen(PORT, () => {
-  console.log(`My first Express app - listening on port ${PORT}!`);
+app.use( (req, res, next) => {
+  res.sendFile(__dirname + "/404.html");
+});
+
+app.listen(8080, () => {
+  console.log("Server is running on port 8080");
 });
