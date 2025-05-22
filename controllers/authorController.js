925af4d4136e13885151522edd356c7c7581e7cf
Raman Sandhu <raman64854@gmail.com>
2025-05-21 23:42:28 -0700

    Implement CustomNotFoundError class for improved error handling in author retrieval

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,25 +1,19 @@
-async function getAuthorById(req, res) {
+const db = require("../db"); // Assuming you have a db module to interact with your databaseexpress-async-handler
+const asyncHandler = require("express-async-handler");
+const CustomNotFoundError = require("../errors/CustomNotFoundError");
+
+// The function will automatically catch any errors thrown and call the next function
+const getAuthorById = asyncHandler(async (req, res) => {
   const { authorId } = req.params;
 
-  try {
-    const author = await db.getAuthorById(Number(authorId));
+  const author = await db.getAuthorById(Number(authorId));
 
-    if (!author) {
-      res.status(404).send("Author not found");
-      return;
-    }
-
-    res.send(`Author Name: ${author.name}`);
-  } catch (error) {
-    console.error("Error retrieving author:", error);
-    res.status(500).send("Internal Server Error");
-
-    // or we can call next(error) instead of sending a response here
-    // Using `next(error)` however will only render an error page in the express' default view and respond with the whole html to the client.
-    // So we will need to create a special type of middleware function if we want a different response and we will get to that in a bit.
+  if (!author) {
+    throw new CustomNotFoundError ("Author not found");
   }
-};
 
+  res.send(`Author Name: ${author.name}`);
+});
 module.exports = {
   getAuthorById, 
 }
--- server.js
+++ server.js
@@ -7,8 +7,18 @@ const indexRouter = require("./routes/indexRouter");
 
 app.use("/authors", authorRouter);
 app.use("/books", bookRouter);
-app.use("/", indexRouter);
+// app.use("/", indexRouter);
 
+app.use((req, res, next) => {
+  throw new Error("OH NO!");
+  // or next(new Error("OH NO!"));
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  // We can now specify the `err.statusCode` that exists in our custom error class and if it does not exist it's probably an internal server error
+  res.status(err.statusCode || 500).send(err.message);
+});
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`My first Express app - listening on port ${PORT}!`);
