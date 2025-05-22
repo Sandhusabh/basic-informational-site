09dac13a618bf71ac41a37044d2afe194f94b802
Raman Sandhu <raman64854@gmail.com>
2025-05-22 01:10:27 -0700

    Add EJS view engine support and create initial views for homepage and navbar

--- server.js
+++ server.js
@@ -4,15 +4,31 @@ const app = express();
 const authorRouter = require("./routes/authorRouter");
 const bookRouter = require("./routes/bookRouter");
 const indexRouter = require("./routes/indexRouter");
+const path = require("node:path");
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
 
 app.use("/authors", authorRouter);
 app.use("/books", bookRouter);
 // app.use("/", indexRouter);
+const links = [
+  { href: "/", text: "Home" },
+  { href: "about", text: "About" },
+];
 
-app.use((req, res, next) => {
-  throw new Error("OH NO!");
-  // or next(new Error("OH NO!"));
+const users = ["Rose", "Cake", "Biff"];
+
+app.get("/", (req, res) => {
+  res.render("index", { links: links, users: users });
 });
+
+// app.use((req, res, next) => {
+//   throw new Error("OH NO!");
+//   // or next(new Error("OH NO!"));
+// });
 
 app.use((err, req, res, next) => {
   console.error(err);
